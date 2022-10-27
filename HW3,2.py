142f855668c8a90bb79a0f9573a09485944ca2b6
ZVINOGRA <116051812+ZVINOGRA@users.noreply.github.com>
2022-10-27 18:23:31 +0300

    Add files via upload

---
+++ HW3,2.py
@@ -0,0 +1,4 @@
+def IsPointInArea(x, y):
+    return((x <= - 3 and x <= 1) and (y <= - 1 and y <= 3))
+result = IsPointInArea(2, 1)
+print(result)
---
+++ HW3.1..py
@@ -0,0 +1,4 @@
+def distance(x1, y1, x2, y2):
+    return((x1**2 - x2**2)**2 + (y1**2 - y2**2)**2)*0.5
+result = distance(2, 2, 3, 3)
+print(result)
